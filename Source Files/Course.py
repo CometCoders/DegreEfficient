65d968535cb758a446b28e4714ca5908c3493a16
aahan96 <axs149430@utdallas.edu>
2015-11-05 18:29:19 -0600

    Matrix working

--- Source Files/Course.py
+++ Source Files/Course.py
@@ -1,10 +1,30 @@
 """The class wrapping a course."""
 
 class Course(object):
-    """Attributes of a course."""
-    name = ""           # The course name.
-    credit = 0          # The number of credit hours. 
-    Id = 0              # Course identifier (not part of input).
-    inDegree = 0        # The number of pre-requisites for this course. 
-    outDegree = 0       # The number of courses for which this is a pre-req.
-    taken = False       # Has the course been taken or not.
+    def __init__(self):
+        self.name = ''           # The course name.
+        self.credit = 0          # The number of credit hours. 
+        self.Id = 0              # Course identifier (not part of input).
+        self.inDegree = 0        # The number of pre-requisites for this course. 
+        self.outDegree = 0       # The number of courses for which this is a pre-req.
+        self.taken = False       # Has the course been taken or not.
+        self.pre_req = []
+    
+    def add_name(self, n):
+        self.name = n
+        
+    def add_credit(self, c):
+        self.credit = c
+
+    def get_name(self):
+        return(self.name)
+
+    def get_credit(self):
+        return(self.credit)
+
+    def add_Id(self, n):
+        self.Id = n
+
+    def get_Id(self):
+        return(self.Id)
+    
--- Source Files/main.py
+++ Source Files/main.py
@@ -1,18 +1,65 @@
 from Course import Course
 
-ask = 'y'
-c = Course()
+ask1 = 'y'
 List = []
 i = 0
 
-while ask=='y':
+while ask1=='y':
     name = raw_input("Enter the course name: ")
     credit = int(raw_input("Enter the credit hours: "))
+    c = Course()
     c.add_name(name)
     c.add_credit(credit)
+    c.Id = i
     List.append(c)
     i = i + 1
-    ask = raw_input("Do you want to enter more? (y/n) : ")
+    ask1 = raw_input("Do you want to enter more? (y/n) : ")
 
-for j in range (0,i):
-    print List[j].get_name(), ", ", List[j].get_credit(), ", ",j
+for c in List:
+    print c.get_name(), ", ", c.get_Id(), ", "
+
+i = 0
+
+print("Enter the pre-reqs for the following courses:\n")
+
+for a in List:
+    prereqs = raw_input(a.name + ": ")
+    if (len(prereqs.split(" ")) == 1):
+        for c in List:
+            if (c.name == prereqs):
+                a.pre_req.append(c)
+    else:
+        for prereq in prereqs.split(" "):
+            for c in List:
+                if (c.name == prereq):
+                    a.pre_req.append(c)
+
+
+Matrix = [[]]
+
+for i in range(0, len(List)):
+    Matrix.append([])
+    for j in range(0, len(List)):
+        Matrix[i].append(0)
+
+for a in List:
+    i = a.Id
+    j = 0
+    for b in a.pre_req:
+       j = int(b.Id)
+       Matrix[i][j] = 1
+
+print("\t"), 
+for a in List:
+    print a.name, 
+print()
+
+
+for a in range(0, len(List)):
+    print (List[a].name + ": "),
+    for b in range(0, len(List)):
+        print (str(Matrix[a][b]) + "\t"),
+    print "\n"
+        
+
+        
