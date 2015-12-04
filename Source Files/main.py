8c991a907e225e787cd8e358351b213f5eb92451
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-12-04 01:56:35 -0600

    Removed debugging-specific output and called display().

--- Source Files/main.py
+++ Source Files/main.py
@@ -4,37 +4,6 @@ from Graph import Graph
 ask1 = 'y'
 List = []
 
-
-"""while ask1=='y':
-    name = raw_input("Enter the course name: ")
-    credit = int(raw_input("Enter the credit hours: "))
-    c = Course()
-    c.add_name(name)
-    c.add_credit(credit)
-    c.Id = i
-    List.append(c)
-    i = i + 1
-    ask1 = raw_input("Do you want to enter more? (y/n) : ")
-
-for c in List:
-    print c.get_name(), ", ", c.get_Id(), ", "
-
-i = 0
-
-print("Enter the pre-reqs for the following courses:\n")
-
-for a in List:
-    prereqs = raw_input(a.name + ": ")
-    if (len(prereqs.split(" ")) == 1):
-        for c in List:
-            if (c.name == prereqs):
-                a.pre_req.append(c)
-    else:
-        for prereq in prereqs.split(" "):
-            for c in List:
-                if (c.name == prereq):
-                    a.pre_req.append(c)"""
-
 f = open("Sample.txt", 'r')
 
 i = 0
@@ -69,11 +38,14 @@ for a in List:
                 if(c.name == name):
                     a.pre_req.append(c)
 
+"""
+STUFF FOR DEBUGGING.
 for a in List:
     print a.name, a.credit,
     for n in a.pre_req_name:
         print n,
     print "\n"
+"""
 
     
 Matrix = []
@@ -90,19 +62,23 @@ for a in List:
        j = int(b.Id)
        Matrix[i][j] = 1
 
+"""
 print("\t"), 
 for a in List:
     print a.name, 
 print()
 
+MORE STUFF FOR DEBUGGING.
 
 for a in range(0, len(List)):
     print (List[a].name + ": "),
     for b in range(0, len(List)):
         print (str(Matrix[a][b]) + "\t"),
     print "\n"
+"""
 
 graph = Graph(List, Matrix)
 graph.sort()
+graph.display()
 
         
